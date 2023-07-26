7a40b1286de99b003dda5929f1d9ad9749368f32
jieun4510@sju.ac.kr <jieun4510@sju.ac.kr>
2023-07-27 00:28:55 +0900

    'FEAT:04-02'

--- usebody/urls.py
+++ usebody/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.UsebodyAPIView.as_view()),
+    path('<int:pk>/', views.UsebodyDetailAPIView.as_view()),
 ]
--- usebody/views.py
+++ usebody/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from usebody.models import Usebody
 from usebody.serializers import UsebodySerializer
 
+from django.shortcuts import get_object_or_404
 
 #04-01 운동 부위 조회
 class UsebodyAPIView(APIView):
@@ -12,3 +13,13 @@ class UsebodyAPIView(APIView):
         serializer = UsebodySerializer(usebody, many=True)
         return Response(serializer.data)
 
+#04-02 운동 부위 상세정보 조회
+class UsebodyDetailAPIView(APIView):
+    #pk에 해당하는 객체 get
+    def get_object(self, pk):
+        return get_object_or_404(Usebody, pk=pk)
+
+    def get(self, request, pk):
+        usebody = self.get_object(pk)
+        serializer = UsebodySerializer(usebody)
+        return Response(serializer.data)
